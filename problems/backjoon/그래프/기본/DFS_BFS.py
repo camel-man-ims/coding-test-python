6bc94541a71256e06b4ce5d82e5aeae71a72fb8b
camel-man-ims <gudwnsrh@gmail.com>
2021-04-18 15:29:55 +0900

    21-04-18 알고리즘

---
+++ problems/backjoon/그래프/기본/DFS_BFS.py
@@ -0,0 +1,45 @@
+# 21.04.18
+
+from collections import deque
+
+def dfs(v):
+    print(v,end = ' ')
+    visited[v] = True
+    for e in adj[v]:
+        if not(visited[e]):
+            dfs(e)
+
+def bfs(v):
+    q = deque([v])
+    while q:
+        v = q.popleft()
+        if not (visited[v]):
+            visited[v]=True
+            print(v,end= ' ')
+            for e in adj[v]:
+                if not visited[e]:
+                    q.append(e)
+
+# n = 정점의 개수 m = 간선의 개수 v = 탐색 시작 번호
+n,m,v = map(int,input().split())
+# 0은 비워둬서 n+1까지 선언
+# adj = 간선. 2차원 배열로 선언
+adj = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    x,y = map(int,input().split())
+    adj[x].append(y)
+    adj[y].append(x)
+
+# 참조값 정렬
+# 문제에서 순서가 낮은 값이 먼저 나오도록 하기 위해
+for e in adj:
+    e.sort()
+
+visited = [False] * (n+1)
+dfs(v)
+print()
+
+visited = [False] * (n+1)
+bfs(v)
+
---
+++ structures/기본/문자열/2차원배열_호출.py
@@ -0,0 +1,7 @@
+# 21.04.18
+arr = [[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]]
+
+for i in range(len(arr)):
+    for num in arr[i]:
+        print(num,end= ' ')
+    print()
